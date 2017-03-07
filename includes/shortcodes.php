2b482c8eb225ba8459f4d076bd092ae84dad0540
Nils Woetzel <nils.woetzel@h-its.org>
2017-03-07 11:44:56 +0100

    added definitions for the shortcode attributes/params -added 'download_id' attribute -added 'attribute' attribute -register with vc_map to the visual composer

--- includes/shortcodes.php
+++ includes/shortcodes.php
@@ -12,54 +12,117 @@ if( !defined( 'ABSPATH' ) ) exit;
 
 class EDD_Github_Shortcodes {
 
-    protected static $permitted_keys = array(
-        'url',
-        'html_url',
-        'assets_url',
-        'upload_url',
-        'tarball_url',
-        'zipball_url',
-        'id',
-        'tag_name',
-        'target_commitish',
-        'name',
-        'body',
-        'draft',
-        'prerelease',
-        'created_at',
-        'published_at',
-    );
+    /**
+     * The json response from the github single releases api has several attributes.
+     * Here, only the attributes are listed, that have a non-complex value.
+     * @see https://developer.github.com/v3/repos/releases/#get-a-single-release
+     *
+     * @access protected
+     * @since  1.2.0
+     * @return string[] array of allowed attribute names
+     */
+    protected static function permittedAttributes() {
+        return array(
+            'url',
+            'html_url',
+            'assets_url',
+            'upload_url',
+            'tarball_url',
+            'zipball_url',
+            'id',
+            'tag_name',
+            'target_commitish',
+            'name',
+            'body',
+            'draft',
+            'prerelease',
+            'created_at',
+            'published_at',
+        );
+    }
 
-//  protected static $download_param = array();
+    /**
+     * This is a shortcode parameter to define the download id.
+     * It adds 'Current' to leave the download_id blank - the shortcode will use the current post.
+     *
+     * @access       protected
+     * @since        1.2.0
+     * @return       array describing a shortcode parameter
+     */
+    protected static function downloadParam() {
+        return array(
+            'param_name' => 'download_id',
+            'heading' => __( 'Specific Download', EDD_Github::TEXT_DOMAIN ),
+            'description' => __( 'This post is used, if you do not select a download.', EDD_Github::TEXT_DOMAIN ),
+            'type' => 'dropdown',
+            'value' => array_merge(
+                           array( __( 'Current Post', EDD_Github::TEXT_DOMAIN ) => ''), // none selectd => use the current post
+                           self::downloads()
+                       ),
+            'admin_label' => true,
+        );
+    }
 
-//  protected static $key_param = array();
+    /**
+     * This is a shortcode parameter to define the json attribute to use.
+     * @see permittedAttributes
+     *
+     * @access       protected
+     * @since        1.2.0
+     * @return       array describing a shortcode parameter
+     */
+    protected static function attributeParam() {
+        return array(
+            'param_name' => 'attribute',
+            'heading' => 'Attribute',
+            'description' => __( 'A single json attribute from the https://developer.github.com/v3/repos/releases/#get-a-single-release response', EDD_Github::TEXT_DOMAIN),
+            'type' => 'dropdown',
+            'value' => self::permittedAttributes(),
+            'std' => 'tag_name',
+            'save_always' => true,
+            'admin_label' => true,
+        );
+    }
 
+    /**
+     * This is the shortcode callback function, registered with 'add_shortcode'.
+     * It adds a shortcode to give direct acces to attribute-value paris in the json response of github single release api call.
+     *
+     * @access       protected
+     * @since        1.2.0
+     * @return       array describing the shortcode
+     */
     protected static function releasesShortcode() {
         return array(
             'name' => __( 'Github Releases', EDD_Github::TEXT_DOMAIN ),
             'base' => 'edd_github_releases',
-            'description' => __( 'Get any single value property of the json response from the single release github api', EDD_Github::TEXT_DOMAIN ),
+            'description' => __( 'Get any non-complex json value from the single release github api call', EDD_Github::TEXT_DOMAIN ),
             'category' => 'EDD',
             'params' => array(
-//              self::downloadParam(),
-//              self::keyParam(),
+                self::downloadParam(),
+                self::attributeParam(),
             )
         );
     }
 
+    /**
+     * construct the class and register the shortcode and add action to register with the visual composer
+     *
+     * @access       public 
+     * @since        1.2.0
+     * @return       EDD_Github_Shortcode
+     */
     public function __construct() {
         add_shortcode( self::releasesShortcode()['base'], array( $this, 'githubReleases') );
         // map shortcodes
-        if( function_exists( 'vc_map' ) ) { 
-            add_action( 'vc_before_init', array( $this, 'vcMap' ) );
-        }
+        add_action( 'vc_before_init', array( $this, 'vcMap' ) );
     }
 
     /**
      * map edd-github shortcodes to visual composer elements
      * http://docs.easydigitaldownloads.com/category/219-short-codes
      *
-     * @access      public since it is registered as an action
+     * @access      public registered as an action
      * @since       1.2.0
      * @return      void
      */
@@ -69,22 +132,26 @@ class EDD_Github_Shortcodes {
     }
 
     /**
-     * get information for a github release in a download
-     * @since 1.2.0
-     * @param $attributes array shortcode attributes
-     * @param 
+     * get information for a github release in a download as shortcode
+     * @see https://codex.wordpress.org/Shortcode_API
+     *
+     * @access      public
+     * @since       1.2.0
+     * @param       $attributes array shortcode attributes
+     * @param       $content string if used with content in the shortcode
+     * @return      string the value for the attribute, or an empty string if there is an error
      */
     public function githubReleases( $attributes, $content = null ) {
         $args = shortcode_atts( array(
-                'download_id' => null,
-                'key' => null,
+                self::downloadParam()['param_name'] => null,
+                self::attributeParam()['param_name'] => null,
             ),
             $attributes,
             self::releasesShortcode()['base']
         );
 
-        // only meaningful with an actual key
-        if ( !in_array($args['key'], self::$permitted_keys)) {
+        // only meaningful with an actual attribute
+        if ( !in_array($args['attribute'], self::permittedAttributes())) {
             return '';
         }
 
@@ -113,7 +180,33 @@ class EDD_Github_Shortcodes {
         $github_releases = new EDD_Github_Releases($github_user, $github_repo, $github_token);
         $release = $github_releases->releases($github_tag);
 
-        return $release->{$args['key']};
+        return $release->{$args['attribute']};
+    }
+
+    /********************
+     * Helper functions *
+     ********************/
+
+    /**
+     * get download posts as array title => id
+     *
+     * @access      protected
+     * @since       1.2.0
+     * @return      array post titel => ID
+     */
+    protected static function downloads() {
+        $posts_array = get_posts(array(
+            'post_type' => 'download',
+            'numberposts' => -1,
+            'orderby' => 'post_title',
+            'order' => 'ASC',
+            'fields' => array('ID','post_title')
+        ));
+        $downloads = array();
+        foreach($posts_array as $post) {
+            $downloads[$post->post_title.'_'.$post->ID] = $post->ID;
+        }
+        return $downloads;
     }
 
 }
